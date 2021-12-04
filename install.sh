8ec540d15b8601ed8ee258d81cd23bdb6c3c5659
LiZnB <liznb@LiZnBs-MacBook-Air.local>
2021-12-05 04:52:48 +0800

    Update

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 set -e
 
-cd ~/.vim_runtime
+cd ~/.vim_runtime/plugged
 
 echo '
 set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/init.vim
 ' > ~/.vimrc
 
-vim -c PlugInstall
+git clone --depth=1 https://github.com/rakr/vim-one 
+git clone --depth=1 https://github.com/Yggdroot/indentLine 
+git clone --depth=1 https://github.com/dense-analysis/ale 
+git clone --depth=1 https://github.com/arcticicestudio/nord-vim 
+
+#vim -c PlugInstall
 
