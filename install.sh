6c601f25da1ae0d62071e842c747fa69189a87b2
LiZnB <liznb@LiZnBs-MacBook-Air.local>
2021-12-10 17:16:47 +0800

    Update

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ git clone --depth=1 https://github.com/fzrkexer/vimrc ~/.vim_runtime
 echo '
 set runtimepath+=~/.vim_runtime
 source ~/.vim_runtime/init.vim
-' > $MYVIMRC
+' > ~/.vimrc
 
-source $MYVIMRC
+source ~/.vimrc
 
 echo "Installation succeeded! ヾ(≧∇≦*)ゝ"
 echo "The plugins are automatically installed the first time Vim starts."
