ed768bda623458d0e4b1a86b6764b6d2e29f2f78
FZRKexEr <51793302+FZRKexEr@users.noreply.github.com>
2021-12-20 22:50:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf ~/.vim_runtime.bak
-mv ~/.vim_runtime/ ~/.vim_runtime.bak/
+cp ~/.vim_runtime ~/.vim_runtime.bak
 cp ~/.vimrc ~/.vimrc.bak
 
 git clone --depth=1 https://github.com/fzrkexer/vimrc ~/.vim_runtime
